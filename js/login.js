0dc808eed76b1e9190e3f4067e9fec25cb741541
redoy-kumar <redoykumar.cse50@gmail.com>
2024-09-19 00:13:10 +0600

    adding all resourse and build login page of adding number and solve the auto reload issue

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById('button-login').addEventListener('click', function(event){
+    // prevent default reload page
+    event.preventDefault();
+
+    const phoneNumber = document.getElementById('phone-number').value;
+})
